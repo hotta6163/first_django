c3c9ba6e1fbb05cc86e7c03e82f24baa49cd62a6
hotta yoshikazu <hotta.sub.1995@gmail.com>
2018-11-02 15:30:21 +0900

    make post_author, post_list

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    return render(request, "blog/post_list.html", {})
+
+def post_author(request):
+    return render(request, "blog/post_author.html", {})
