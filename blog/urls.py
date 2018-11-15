36e5816c9a1a368d9df244e7ec5a8cd045fc31a9
LAPTOP-SD3CKV3O\yoshi <hotta.programing.2018@gmail.com>
2018-11-15 10:36:24 +0900

    organize template

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('author/', views.post_author, name='post_author')
+    path('author/', views.post_author, name='post_author'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -10,3 +10,7 @@ def post_list(request):
 
 def post_author(request):
     return render(request, "blog/post_author.html", {})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
